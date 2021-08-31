15b46f1efe9d7966c4c14a4b10a64bb0ebaac92a
David Muñoz <dmbdavid97@gmail.com>
2021-08-31 20:30:28 +0200

    remove videos

---
+++ web/removeTimelapses.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+path="/app/static_build/media/tsd-timelapses/private"
+path2="/app/static_build/media/tsd-pics"
+
+rm -rf $path
+mkdir $path
+
+rm -rf $path2
+
+echo "TimeLapses removed"
